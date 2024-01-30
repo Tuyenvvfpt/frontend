855b24758f96fbeaba9eb04f45a5dc6d16043cf3
Tuyenvvfpt <vunhatvinh2006@gmail.com>
2024-01-31 06:58:02 +0700

    f8 javascript hàm

--- lesson-1/main3.js
+++ lesson-1/main3.js
@@ -6,8 +6,33 @@
 var a = 1;
 var b = "1";
 //only comparison by data
-console.log(a == b);
-console.log(!(a != b));
+console.log(a == b); //true
+console.log(!(a != b)); //true
 
 //comparison by data and dataType
-console.log(a === b);
+console.log(a === b); //false
+
+/*
+logical and if
+*/
+var c = 2;
+
+// var result = "A" && "B" && "C";
+// console.log(result);
+
+// var conclusion = null && "B" && "C";
+// if (conclusion) {
+//     console.log("dieu kien dung");
+// } else {
+//     console.log("dieu kien sai");
+// }
+
+var result2 = "A" || "B" || "C";
+console.log(result2);
+
+var conclusion2 = null || "B" || "C";
+if (conclusion2) {
+    console.log("dieu kien dung");
+} else {
+    console.log("dieu kien sai");
+}
---
+++ lesson-1/main4.js
@@ -0,0 +1,122 @@
+/* 1. hàm?
+- một khối mã
+- làm một việc cụ thể
+2. loại hàm:
+- built- in
+- tự định nghĩa
+3. tính chất:
+- không thực thi khi định nghĩa
+- sẽ thực thi khi dược gọi
+- có thể nhận tham số
+- có thể trả về một giá trị
+4. tạo hàm đầu tiên
+*/
+
+// function showDialog() {
+//     alert("hello world");
+// }
+
+// showDialog();
+
+/*
+1. tham số?
+- định nghĩa
+- kiểu dữ liệu
+- tính private //hoạt động trong hàm
+- một tham số
+- nhiều tham số
+2. truyền tham số
+- một tham số
+- nhiều tham số
+3. arguments?
+- đối tượng arguments
+- giới thiệu vòng for
+*/
+//1
+// function writeLog(message) {
+//     console.log(message);
+// }
+// writeLog("hello");
+
+// function writeType(message) {
+//     console.log(typeof message);
+// }
+// writeType(["array1, array2"]);
+
+//2
+// function writeJob(message1, mess2) {
+//     console.log(message1);
+//     console.log(mess2); //undefined
+// }
+// writeJob("tester");
+
+//3
+// function writeArguments() {
+//     console.log(arguments);
+// }
+// writeArguments("q", "w", "e", "r", "t", "y");
+
+//4 important
+function writeArray() {
+    var myString = "";
+    for (var param of arguments) {
+        // console.log(param);
+        myString += `${param} - `
+    }
+    console.log(myString);
+}
+writeArray("q", "w", "e", "r", "t", "y", 1, 2, 3);
+
+/*Return 
+*/
+// console.warn("Return trong hàm")
+// var isConfirm = confirm("Message?");
+// console.log(isConfirm);
+
+// function add(a, b) {
+//     return a + b; //nếu không có return sẽ là undefined
+// }
+// var addResult = add(1, 2);
+// console.log(addResult);
+
+
+// console.warn("Demo nối chuỗi")
+// function addDemo(a, b) {
+//     return "1" + "và"; 
+// }
+// var demo = addDemo(1, 2);
+// console.log(demo);
+
+/* 
+Các loại function
+1. declare function
+hosting: có thể gọi trước khi được định nghĩa
+2. expression function
+3. arrow function
+*/
+// 1. declare function
+function declareFunction() {
+
+}
+
+// 2. expression function
+var expressionFunction = function testName() {
+
+}
+
+setTimeout(function autoLogout() {
+
+}, 2000);
+
+var myObject = {
+    myFunction1: function () {
+
+    }
+}
+
+// DEMO
+test1();
+
+function test1() {
+    console.log("test hosting");
+}
