aff9158f2bd5173aa5af38521ca5351e2a644939
Tuyenvvfpt <vunhatvinh2006@gmail.com>
2024-02-02 17:05:09 +0700

    f8_javascript_8_object

--- lesson-1/main8.js
+++ lesson-1/main8.js
@@ -1,4 +1,5 @@
 /* Object trong Javascripr  */
+console.warn("Object")
 var emailKey = "emailEdu";
 var myInfor = {
     name: "Vu Van Tuyen",
@@ -32,5 +33,69 @@ console.warn("check type:...")
 console.log(typeof phoneNumber); //undefined
 
 /* Object Constructor */
+console.warn("Object Constructor")
+function User1(firstName, lastName, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+    this.getName = function () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
 
+var User2 = function (firstName, lastName, avatar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+    this.getName = function () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+var author = new User1("Tuyen", "Vu", "avt1");
+var admin = new User1("Nam", "Cao", "avt2");
+console.log(author);
+console.log(admin);
+console.log(author.constructor == User1); //true
+
+
+author.title = "chia se dao tai F8";
+admin.level = "high";
+console.log(author);
+console.log(admin);
+
+console.log(admin.getName());
+
+/* Object prototype - Basic
+1. La gi?
+2. Su dung khi nao */
+console.warn("Object Prototype");
+
+User1.prototype.className = "F8";
+console.log(author);
+
+User1.prototype.getClassName = function () {
+    return this.className;
+}
+
+/* ĐỐI TƯỢNG DATE
+Search: Javascript Date Object Mozilla */
+console.warn("Date");
+var date = new Date();
+var date2 = Date();
+
+console.log(typeof date); //object
+console.log(typeof date2); //string
+
+console.log("Today is: " + date);
+
+var year = date.getFullYear();
+console.log("Year is: " + year);
+
+var month = date.getMonth();
+console.log("Month is: " + month + 1);
+
+var day = date.getDate();
+console.log("Day is: " + day);
+
+console.log(`${day}/${month}/${year}`);
 
